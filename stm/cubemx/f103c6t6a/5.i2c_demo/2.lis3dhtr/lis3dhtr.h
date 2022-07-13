9a90122a69246482a127b182ec55cff88a5801e1
uyk <2397410949@qq.com>
2022-07-13 13:39:45 +0800

    lis3dhtr

---
+++ stm/cubemx/f103c6t6a/5.i2c_demo/2.lis3dhtr/lis3dhtr.h
@@ -0,0 +1,75 @@
+#ifndef _LIS3DHTR_H_
+#define _LIS3DHTR_H_
+
+#include <math.h>
+
+#include "i2c.h"
+#include "usart.h"
+
+#define LIS3DHTR_ADDR_LOW 0x18
+#define LIS3DHTR_ADDR_HIGH 0x19
+
+#define LIS3DHTR_DEV 0x32   // dev
+#define LIS3DHTR_I2C hi2c1  // hi2c
+
+#define LIS3DHTR_WHO_AM_I 0x33
+
+// Register addresses
+
+#define LIS3DHTR_STATUS_REG_AUX 0x07
+#define LIS3DHTR_OUT_ADC1_L 0x08
+#define LIS3DHTR_OUT_ADC1_H 0x09
+#define LIS3DHTR_OUT_ADC2_L 0x0A
+#define LIS3DHTR_OUT_ADC2_H 0x0B
+#define LIS3DHTR_OUT_ADC3_L 0x0C
+#define LIS3DHTR_OUT_ADC3_H 0x0D
+#define LIS3DHTR_INT_COUNTER_REG 0x0E
+#define LIS3DHTR_WHO_AM_I_REG 0x0F
+#define LIS3DHTR_TEMP_CFG_REG 0x1F
+#define LIS3DHTR_CTRL_REG1 0x20
+#define LIS3DHTR_CTRL_REG2 0x21
+#define LIS3DHTR_CTRL_REG3 0x22
+#define LIS3DHTR_CTRL_REG4 0x23
+#define LIS3DHTR_CTRL_REG5 0x24
+#define LIS3DHTR_CTRL_REG6 0x25
+#define LIS3DHTR_REFERENCE 0x26
+#define LIS3DHTR_STATUS_REG2 0x27
+#define LIS3DHTR_OUT_X_L 0x28
+#define LIS3DHTR_OUT_X_H 0x29
+#define LIS3DHTR_OUT_Y_L 0x2A
+#define LIS3DHTR_OUT_Y_H 0x2B
+#define LIS3DHTR_OUT_Z_L 0x2C
+#define LIS3DHTR_OUT_Z_H 0x2D
+#define LIS3DHTR_FIFO_CTRL 0x2E
+
+#define LIS3DHTR_CLICKCFG_REG 0x38
+#define LIS3DHTR_CLICKSRC_REG 0x39
+#define LIS3DHTR_CLICKTHS_REG 0x3A
+#define LIS3DHTR_TIMELIMIT_REG 0x3B
+#define LIS3DHTR_TIMELATENCY_REG 0x3C
+#define LIS3DHTR_TIMEWINDOW_REG 0x3D
+
+// Register value constants:
+#define LIS3DHTR_RANGE_16_G 3        // ±16g, divider = 1365
+#define LIS3DHTR_RANGE_8_G 2         // ±8g,  divider = 4096
+#define LIS3DHTR_RANGE_4_G 1         // ±4g,  divider = 8190
+#define LIS3DHTR_RANGE_2_G 0         // ±2g,  divider = 16380
+#define LIS3DHTR_DATARATE_1344_HZ 9  // 1.344 KHz
+#define LIS3DHTR_DATARATE_400_HZ 7   // 400Hz
+#define LIS3DHTR_DATARATE_200_HZ 6   // 200Hz
+#define LIS3DHTR_DATARATE_100_HZ 5   // 100Hz
+#define LIS3DHTR_DATARATE_50_HZ 4    // 50Hz
+#define LIS3DHTR_DATARATE_25_HZ 3    // 25Hz
+#define LIS3DHTR_DATARATE_10_HZ 2    // 10 Hz
+#define LIS3DHTR_DATARATE_1_HZ 1     // 1 Hz
+#define LIS3DHTR_DATARATE_POWERDOWN 0
+#define LIS3DHTR_DATARATE_LOWPOWER_1K6HZ 8
+#define LIS3DHTR_DATARATE_LOWPOWER_5KHZ 9
+
+#define STANDARD_GRAVITY 9.806
+#define RAD_TO_DEG 57.29
+
+void LIS3DHTR_Init(void);
+void LIS3DHTR_readAccel(float* x, float* y, float* z);
+
+#endif
